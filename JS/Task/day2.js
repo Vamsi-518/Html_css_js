5c4c327042e592aae2f1bf2109d6991f8f0e901e
vamsi <chmnvkrishna@gmail.com>
2026-03-11 12:35:07 +0530

    first commit

---
+++ JS/Task/day2.js
@@ -0,0 +1,27 @@
+var a=13
+var b=13
+var op = '*'
+switch (op) {
+    case '+':
+        console.log("the addition of "+a+ " and " + b +" is "+(a+b));
+         break;
+    case '-':
+        console.log("the sub of "+a+ " and " +b+" is "+ (a-b));
+        break;
+    case '/':
+        console.log("the division  of "+a+ " and " + b +" is " +(a/b));
+        break;
+    case '*':
+        console.log("the mutli of "+a+ " and " + b +" is " +(a*b));
+        break;
+    case '%':
+        console.log("the modulous of "+a+ " and " + b +" is " +(a%b));
+        break;
+    default:
+        console.log();
+        break; 
+    }
+
+
+
+
