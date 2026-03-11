0fab7ecc6c3ac69be3b32eb584b7295b54f7ebee
vamsi <chmnvkrishna@gmail.com>
2026-03-11 12:42:13 +0530

    first commit

---
+++ jsday6/day6.js
@@ -0,0 +1,22 @@
+
+// const changeMe = () => {
+//     let heading = document.getElementById("heading")
+//     heading.innerHTML = "Vamsi"
+// } 
+
+const changeMe = () => {
+    let heading = document.getElementById("heading") 
+    heading.style.color ="white"
+    heading.style.backgroundColor = "black"
+    heading.style.width="fit-content"
+
+
+}
+
+const ShowImage= () => {
+     let box = document.getElementsByClassName("container")[0]
+
+     box.style.backgroundImage = 'url("https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExaHR6M3ZkaDY0NHUwMGd4bGh5YTNmdWcycGF4ZXd1cGV5YjFlb3dhciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/6IwBH2VIF3PlXHaWbM/giphy.gif")'
+    
+     
+}
