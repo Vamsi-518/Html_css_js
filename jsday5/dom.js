aa170637860e4331f30c1083bc001efd68118865
vamsi <chmnvkrishna@gmail.com>
2026-03-11 12:41:58 +0530

    first commit

---
+++ jsday5/dom.js
@@ -0,0 +1,29 @@
+//Dom
+
+console.log('buchi')
+
+let display = document.getElementById("display")
+
+display.textcontent = "This ia <b>Vamsi</b>"
+
+display.innerHTML = "This is <b>VAMSI</b>"
+
+
+
+
+
+document.getElementById("display".innerHTML = "THis is javascript")
+
+let paras = document.getElementsByTagName("p")
+
+// // // // let worldpara = paras[2]
+
+console.log(paras);
+
+
+
+
+
+let homeparas = document.getElementsByClassName('home')
+homeparas[1].innerHTML = "hello bye"
+console.log(homeparas)
