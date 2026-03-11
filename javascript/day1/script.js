b9fe73137f4e9da02c058760af97e5ff536065ef
vamsi <chmnvkrishna@gmail.com>
2026-03-11 12:40:56 +0530

    first commit

---
+++ javascript/day1/script.js
@@ -0,0 +1,13 @@
+console.log("Hello javascript vamsi");
+var num=true
+console.log(num + " is a " + typeof num);
+
+var a=26
+var b='26'
+if(a+b){
+    console.log("yes")
+}else {
+    console.log("No")
+}
+var c=a+b
+console.log(c)
